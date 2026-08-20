f5e2e7558a44fa9bf105b7738d25c000077002bf
MasayaAUT <61126271+MasayaAUT@users.noreply.github.com>
2026-08-20 15:03:52 +1200

    PleaseWork

--- MGA/Build/BUILD.loader.js
+++ MGA/Build/BUILD.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 79688565,
+    wasmFileSize: 79688666,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
